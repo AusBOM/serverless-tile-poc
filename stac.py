092d146cfc466bd80b2b4286a4d967e65515222f
Michael Bell <michael.bell@bom.gov.au>
2019-05-27 11:47:14 +1000

    Update deployment

--- stac.py
+++ stac.py
@@ -1,6 +1,7 @@
 import boto3
 import json
 from decimal import Decimal
+from boto3.dynamodb.conditions import Key, Attr
 
 # Helper class to convert a DynamoDB item to JSON.
 
@@ -14,33 +15,51 @@ def update_stac(item):
 
 def handler(event, context):
 
-  # with open("stac/{datatype}.json".format(datatype=data)) as stac_file:
-  #   stac_config = json.load(stac_file)
-  print(event)
-  print(event['body'])
-  stacs = json.loads(event['body'], parse_float=Decimal)
+    # with open("stac/{datatype}.json".format(datatype=data)) as stac_file:
+    #   stac_config = json.load(stac_file)
+    print(event)
+    print(event['body'])
+    stacs = json.loads(event['body'], parse_float=Decimal)
 
-  for stac in stacs:
-    update_stac(stac)
+    for stac in stacs:
+        update_stac(stac)
+        body = get_collection(collection_id)
+    
+    return {
+        'statusCode': "204",
+        'headers': {},
+        }
+
 
 def get_stac(id):
-  print('hi')
-  
-  dynamodb = boto3.resource('dynamodb', region_name='ap-southeast-2')
-  table = dynamodb.Table('stac_register')
-  response = table.get_item(
-    Key={
-      'id': id
-    })
-  # return json.dumps("[{  \"type\": \"Feature\",  \"id\" : \"RAINRATE\",  \"bbox\": [140.7495166666666666, -13.823025, 143.09648333333333332,  -11.5096694444444444],  \"geometry\": {    \"type\": \"Polygon\",    \"coordinates\": [      [        [140.7495166666666666,  -13.823025],        [140.7495166666666666, 143.09648333333333332],        [143.09648333333333332, 143.09648333333333332],        [143.09648333333333332,  -13.823025],        [140.7495166666666666,  -13.823025]      ]    ]  },  \"properties\": {    \"datetime\": \"2018-06-26T00:00:0.000Z\",    \"min\": 0,    \"max\": 1200,    \"mask_min\": \"true\",    \"scale_factor\": 0.05,    \"style_thresholds\": [0, 4, 10, 30, 50, 80, 120, 200, 300, 400, 700, 1200]  },  \"collection\": \"RAINRATE\",  \"links\": [  ],  \"assets\": {    \"source\": {      \"href\": \"s3://bom-csiro-serverless-test/abcout.tif\",      \"title\": \"uv\",      \"product\": \"this\"    }  }}]")
-  print(response['Item'], type(response['Item']))
-  # item = response['Item']
-  # print(item)
-  # return item
-  return replace_decimals(response['Item'])
-  # return response['Item']
 
-# get_stac('RAINRATE')
+    dynamodb = boto3.resource('dynamodb', region_name='ap-southeast-2')
+    table = dynamodb.Table('stac_register')
+    response = table.get_item(
+        Key={
+        'id': id
+        })
+    return replace_decimals(response['Item'])
+
+
+def collection_items_handler(event, context):
+    body = get_collection(event['pathParameters']['collection_id'])
+    return {
+    'statusCode': "200",
+    'headers': {
+      'Content-Type': 'application/json'
+    },
+    'body': body,
+  }
+
+def get_collection(collection_id):
+    dynamodb = boto3.resource('dynamodb', region_name='ap-southeast-2')
+    table = dynamodb.Table('stac_register')
+    filter_expression = Key('collection').eq(collection_id)
+    response = table.scan(
+        FilterExpression=filter_expression,
+    )
+    return replace_decimals(response['Items'])
 
 # https://github.com/boto/boto3/issues/369
 def replace_decimals(obj):
